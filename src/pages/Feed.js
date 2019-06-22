614b4529931ff0cca7f38163ca4b260574dbf1da
root <root@DESKTOP-77RQ0M5.localdomain>
2019-06-22 01:06:39 -0300

    class 3 completed

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,36 +1,73 @@
 import React, { Component } from 'react';
 import './Feed.css';
-
+import api from '../services/api';
+import io from 'socket.io-client';
 import more from '../assets/more.svg';
 import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 import like from '../assets/like.svg';
 
 class Feed extends Component {
+	state = {
+		feed: []
+	};
+
+	registerToSocket = () => {
+		const socket = io('http://localhost:3333');
+
+		socket.on('post', newPost => {
+			this.setState({ feed: [newPost, ...this.state.feed] });
+		});
+
+		socket.on('like', likedPost => {
+			this.setState({
+				feed: this.state.feed.map(post =>
+					post._id === likedPost._id ? likedPost : post
+				)
+			});
+		});
+	};
+
+	async componentDidMount() {
+		this.registerToSocket();
+		const response = await api.get('posts');
+
+		this.setState({ feed: response.data });
+	}
+
+	handleLike = id => {
+		api.post(`posts/${id}/like`);
+	};
+
 	render() {
 		return (
 			<section id="post-list">
-				<article>
-					<header>
-						<div className="user-info">
-							<span>Lucas Rego</span>
-							<span className="place">Aracaju</span>
-						</div>
-						<img src={more} alt="" />
-					</header>
-					<img src="http://localhost:3333/files/profile.jpg" alt="" />
-					<footer>
-						<div className="actions">
-							<img src={like} alt="" />
-							<img src={comment} alt="" />
-							<img src={send} alt="" />
-						</div>
-						<strong>900 curtidas</strong>
-						<p>
-							Um post legal<span>#hashtags</span>
-						</p>
-					</footer>
-				</article>
+				{this.state.feed.map(post => (
+					<article>
+						<header>
+							<div className="user-info">
+								<span>{post.author}</span>
+								<span className="place">{post.place}</span>
+							</div>
+							<img src={more} alt="" />
+						</header>
+						<img src={`http://localhost:3333/files/${post.image}`} alt="" />
+						<footer>
+							<div className="actions">
+								<button type="submit" onClick={() => this.handleLike(post._id)}>
+									<img src={like} alt="" />
+								</button>
+								<img src={comment} alt="" />
+								<img src={send} alt="" />
+							</div>
+							<strong>{post.likes} curtidas</strong>
+							<p>
+								{post.description}
+								<span>{post.hashtags}</span>
+							</p>
+						</footer>
+					</article>
+				))}
 			</section>
 		);
 	}
